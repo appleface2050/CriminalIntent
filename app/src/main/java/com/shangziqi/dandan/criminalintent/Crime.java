06892f81dba87514fc4f2e1de73ee4350274149b
andy-bst <andy@bluestacks.com>
2017-05-27 12:26:02 +0800

    i

---
+++ app/src/main/java/com/shangziqi/dandan/criminalintent/Crime.java
@@ -0,0 +1,43 @@
+package com.shangziqi.dandan.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by andy-bst on 2017/5/27.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
