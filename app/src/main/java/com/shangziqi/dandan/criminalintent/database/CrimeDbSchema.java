9c1f11c2957de29bebae36e9dd9ab4d9ec3baac1
andy-bst <andy@bluestacks.com>
2017-06-08 11:48:24 +0800

    step done

---
+++ app/src/main/java/com/shangziqi/dandan/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,18 @@
+package com.shangziqi.dandan.criminalintent.database;
+
+/**
+ * Created by andy-bst on 2017/6/8.
+ */
+
+public class CrimeDbSchema {
+    public static final class CrimeTable {
+        public static final String NAME = "crimes";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
